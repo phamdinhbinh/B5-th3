8cc8764d422b6b8cc71919ec8301086931131b65
phamdinhbinh <phamdinhbinhonly1@gmail.com>
2023-02-17 14:05:16 +0700

    th3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { useState, useMemo } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [name, setName] = useState();
+  const [price, setPrice] = useState();
+  const [products, setProducts] = useState([]);
+
+  const total = useMemo(() => {
+    const result = products.reduce((acc, ele) => {
+      
+      console.log('Tính toán lại ...');
+      return acc + ele.price;
+    }, 0);
+    return result;
+  }, [products]);
+
+
+  const onAddProduct = () => setProducts([...products, { name, price: Number(price) }]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <input
+        placeholder='Enter name...'
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      <br />
+      <input
+        placeholder='Enter price...'
+        value={price}
+        onChange={e => setPrice(e.target.value)}
+      />
+      <div onClick={onAddProduct}>Add product</div>
+      <div>Total: {total}</div>
+      <ul>
+        {products.map((e, i) => <li key={i}>{e.name} - {e.price}</li>)}
+      </ul>
     </div>
   );
 }
-
-export default App;
